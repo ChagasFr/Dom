4eef16996a6a5cb143bc612bfe939de55e845f8f
Felipe chagas <felipereis9998@aluno.saojudas.br>
2023-12-30 18:59:03 -0300

    feat: desenvolvimento barreiras in game

--- js/flappy.js
+++ js/flappy.js
@@ -9,4 +9,20 @@ function Barreira(reversa = false) {
 
     const borda = novoElemento('div', 'borda')
     const corpo = novoElemento('div', 'corpo')
+    this.elemento.appendChild(reversa ? corpo : borda)
+    this.elemento.appendChild(reversa ? borda : corpo)
+
+    this.setAltura = altura => corpo.style.height = `${altura}px`
+}
+
+// const b = new Barreira(true)
+// b.setAltura(300)
+// document.querySelector('[wm-flappy]').appendChild(b.elemento)
+
+function ParDeBarreiras(altura, abertura, x) {
+    this.elemento = novoElemento('div', 'par-de-barreiras')
+
+    // barreiras
+    this.superior = new Barreira(true)
+    this.inferior = new Barreira(false)
 }
